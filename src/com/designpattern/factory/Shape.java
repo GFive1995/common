5effbdd5e5dcdced624e8e6d1c7db03b79144b14
wangchenyang <GFive1995@163.com>
2019-06-04 11:31:30 +0800

    工厂模式

---
+++ src/com/designpattern/factory/Shape.java
@@ -0,0 +1,6 @@
+package com.designpattern.factory;
+
+
+public interface Shape {
+	void draw();
+}
---
+++ src/com/designpattern/factory/Square.java
@@ -0,0 +1,10 @@
+package com.designpattern.factory;
+
+
+public class Square implements Shape {
+
+	public void draw() {
+		System.out.println("初始化正方形");
+	}
+
+}

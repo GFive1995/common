1fd8d1409e05c52d94060cd1dea44ef871246b26
wangchenyang <GFive1995@163.com>
2019-06-04 10:32:21 +0800

    单例模式

---
+++ src/com/designpattern/single/Test.java
@@ -0,0 +1,23 @@
+package com.designpattern.single;
+
+
+public class Test {
+	
+	public static void main(String[] args) {
+		EagerSingleton eagerSingleton1, eagerSingleton2;
+		eagerSingleton1 = EagerSingleton.getInstance();
+		eagerSingleton2 = EagerSingleton.getInstance();
+		System.out.println(eagerSingleton1 == eagerSingleton2);
+		
+		LazySingleton lazySingleton1, lazySingleton2;
+		lazySingleton1 = LazySingleton.getInstance();
+		lazySingleton2 = LazySingleton.getInstance();
+		System.out.println(lazySingleton1 == lazySingleton2);
+		
+		StaticInnerSingleton staticInnerSingleton1, staticInnerSingleton2;
+		staticInnerSingleton1 = StaticInnerSingleton.getInstance();
+		staticInnerSingleton2 = StaticInnerSingleton.getInstance();
+		System.out.println(staticInnerSingleton1 == staticInnerSingleton2);
+	}
+	
+}

3668bb3b34c6eb9eff745c47f5bc578219c254d1
王晨仰 <GFive1995@163.com>
2019-08-06 14:20:27 +0800

    final

--- src/com/basics/Final.java
+++ src/com/basics/Final.java
@@ -5,21 +5,25 @@ package com.basics;
  * final
  * 1、基本数据类型
  * 		对于基本数据类型final使数值恒定不变。
- * 		static + final 表示只占据一段不能改变的存储空间。
- *  	参数列表中声明final，意味着无法在方法中更改参数引用所指向的对象。(主要用来向匿名内部类传递数据)
- * 2、方法
+ * 2、引用类型
+ * 		表示引用的地址是不能改变的。
+ * 3、方法
  * 		可以把方法锁住，防止任何继承类修改它的含义。
  * 		统一编译器将针对该方法的所有调用都转换为内嵌调用。
  * 		所有private方法隐式地指为final。
- * 3、类
+ * 4、类
  * 		表示此类不能被继承。(String)
  * 
  * @version 1.0
  */
 public class Final {
 
-	private final String ATTRIBUTE_A = "final";
-	private static final String ATTRIBUTR_B = "staticFinal";
+	private final int AttRIBUTE_A = 0;
+	private final String ATTRIBUTR_B = "final";
+	/**
+	 * static + final 表示只占据一段不能改变的存储空间。
+	 */
+	private static final String ATTRIBUTE_C = "staticFinal";
 	
 	public final void Method() {
 		System.out.println("final Method");
@@ -30,6 +34,9 @@ public class Final {
 	}
 	
 	public static void main(String[] args) {
+		/**
+		 * 参数列表中声明final，意味着无法在方法中更改参数引用所指向的对象。(主要用来向匿名内部类传递数据)
+		 */
 		final String attributeC = "匿名内部类";
 		new Thread(new Runnable() {
 			public void run() {

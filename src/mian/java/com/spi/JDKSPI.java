d0a85c60038e8eb24b8ffe76a15062c94afd0ee2
王晨仰 <GFive1995@163.com>
2019-08-08 17:21:20 +0800

    spi

--- src/mian/java/com/spi/JDKSPI.java
+++ src/mian/java/com/spi/JDKSPI.java
@@ -8,13 +8,8 @@ import java.util.ServiceLoader;
  * SPI的本质是将接口实现类的全限定名配置在文件中，并由服务加载器读取配置文件，加载实现类。
  * 
  * @version 1.0
- * @since JDK1.7
- * @author wangcy
- * @company 上海朝阳永续信息技术有限公司
- * @copyright (c) 2019 SunTime Co'Ltd Inc. All rights reserved.
- * @date 2019年8月8日 下午2:27:09
  */
-public class SPITest {
+public class JDKSPI {
 
 	public static void main(String[] args) {
 		ServiceLoader<Robot> serviceLoader = ServiceLoader.load(Robot.class);
